ed0d1039b1732698b165990d072002bfd5347793
Snek <97262062+sn3kdev@users.noreply.github.com>
2022-01-07 10:17:37 +0200

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-// Выбираем кнопку
-const btn = document.querySelector(".theme-button");
-// Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function () {
-  // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-  document.body.classList.toggle("dark-theme");
-});
