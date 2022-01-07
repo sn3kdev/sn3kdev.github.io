28543b9d0f75c346a8b319f99ca79079992aaa91
DoomGod <70023601+elezor-dev@users.noreply.github.com>
2022-01-07 06:01:12 +0500

    Update java.js

--- java.js
+++ java.js
@@ -2,6 +2,6 @@ let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 
 themeButton.onclick = function() {
-  page.classList.toggle('light-theme');
-  page.classList.toggle('dark-theme');
+  page.classList.toggle('light');
+  page.classList.toggle('dark');
 };
